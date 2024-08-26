93c421f01634ff9631900bc7c317b1e73f74feca
Daniel <daniel.gallo12@gmail.com>
2024-08-25 19:13:06 -0700

    Add axios and test api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const getWeather = async () => {
+    const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`);
+    console.log(response);
+  }
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
